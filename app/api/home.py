512bd9e637ff4897c0c72268c9a41bb7d83c3b38
Jimmy Man <man.trandm@watasolutions.com>
2020-07-07 11:01:24 +0700

    response url

--- app/api/home.py
+++ app/api/home.py
@@ -16,10 +16,10 @@ class HomeAPI:
         self.comics = model.comics
     
     def CtrlGet(self):
-        randomDb = self.comics.aggregate( [ { "$sample": { "size" : 12} }, { "$project": { "_id": 1, "name": 1, "cover": 1 } } ] )  
-        latestDb = self.comics.find({},{ "_id": 1, "name": 1, "cover": 1 }).sort("updatedAt", -1).limit(12)
-        hotestDb = self.comics.find({ "isHot": True },{ "_id": 1, "name": 1, "cover": 1 }).limit(12)
-        completedDb = self.comics.find({ "status": 1 },{ "_id": 1, "name": 1, "cover": 1 }).limit(12)
+        randomDb = self.comics.aggregate( [ { "$sample": { "size" : 12} }, { "$project": { "_id": 1, "name": 1, "cover": 1, "url": 1 } } ] )  
+        latestDb = self.comics.find({},{ "_id": 1, "name": 1, "cover": 1, "url": 1 }).sort("updatedAt", -1).limit(12)
+        hotestDb = self.comics.find({ "isHot": True },{ "_id": 1, "name": 1, "cover": 1, "url": 1 }).limit(12)
+        completedDb = self.comics.find({ "status": 1 },{ "_id": 1, "name": 1, "cover": 1, "url": 1 }).limit(12)
 
         random = list(randomDb)
         completed = list(completedDb)
